0a772f4db3c97fe4680baf376e19c1cc84f8802d
Cherry man 1 <kwc94@live.missouristate.edu>
2018-04-30 18:23:20 -0500

    Added more in the main for the timer stuff

--- app/src/main/java/com/cornez/employeetracker/MainActivity.java
+++ app/src/main/java/com/cornez/employeetracker/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cornez.employeetracker;
 
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -42,6 +44,36 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    public void startTimer(View view){
+        stopBtn.setEnabled(true);
+        startBtn.setEnabled(false);
+        resetBtn.setEnabled(false);
+
+        watchTime.setStartTime(SystemClock.uptimeMillis());
+        mHandler.postDelayed(updateTimerRunnable, 20);
+    }
+
+    private Runnable updateTimerRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+            timeInMilliseconds = SystemClock.uptimeMillis() - watchTime.getStartTime();
+
+            watchTime.setTimeUpdate(watchTime.getmStoredTime() + timeInMilliseconds);
+
+            int time = (int) (watchTime.getTimeUpdate()/1000);
+
+            int minutes = time / 60;
+            int seconds = time % 60;
+            int milliseconds = (int) (watchTime.getTimeUpdate() % 1000);
+
+            timeDisplay.setText(String.format("%02d",minutes)+":"+String.format("%02d",seconds)+
+                    ":"+String.format("%02d", milliseconds));
+
+            mHandler.postDelayed(this, 0);
+
+        }
+    };
 
 
 }
