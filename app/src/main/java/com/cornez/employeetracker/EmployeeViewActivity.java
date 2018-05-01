c8410a60c3c5998f86859aed26f56bc6a2486042
Colin Gillette <colingillette35@gmail.com>
2018-04-30 21:34:04 -0500

    All layouts except Main should link appropriately

--- app/src/main/java/com/cornez/employeetracker/EmployeeViewActivity.java
+++ app/src/main/java/com/cornez/employeetracker/EmployeeViewActivity.java
@@ -1,8 +1,46 @@
 package com.cornez.employeetracker;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class EmployeeViewActivity extends AppCompatActivity
 {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.employee_view);
+    }
 
+    //Transition between layouts
+    final Button btnIn = (Button) findViewById(R.id.empViewInButton);
+    final Button btnOut= (Button) findViewById(R.id.empViewOutButton);
+
+    public void changeLayout(View view)
+    {
+        View.OnClickListener handler = new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                if (v == btnIn)
+                {
+                    Intent intentMain = new Intent(EmployeeViewActivity.this,
+                            EmployeeViewActivity.class);
+                    EmployeeViewActivity.this.startActivity(intentMain);
+                }
+                if (v == btnOut)
+                {
+                    Intent intentMain = new Intent(EmployeeViewActivity.this,
+                            EmployeeViewActivity.class);
+                    EmployeeViewActivity.this.startActivity(intentMain);
+                }
+            }
+        };
+
+        btnIn.setOnClickListener(handler);
+        btnOut.setOnClickListener(handler);
+    }
 }
--- app/src/main/java/com/cornez/employeetracker/EmployerTaskActivity.java
+++ app/src/main/java/com/cornez/employeetracker/EmployerTaskActivity.java
@@ -1,8 +1,50 @@
 package com.cornez.employeetracker;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class EmployerTaskActivity extends AppCompatActivity
 {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.employee_view);
+    }
 
+    //Transition between layouts
+    final Button btnClear = (Button) findViewById(R.id.taskViewClearButton);
+    final Button btnEdit = (Button) findViewById(R.id.taskViewEditButton);
+    final Button btnBack = (Button) findViewById(R.id.taskViewBackButton);
+
+    public void changeLayout(View view)
+    {
+        View.OnClickListener handler = new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                if (v == btnClear)
+                {
+
+                }
+                if (v == btnEdit)
+                {
+
+                }
+                if (v == btnBack)
+                {
+                    Intent intentMain = new Intent(EmployerTaskActivity.this,
+                            EmployerViewActivity.class);
+                    EmployerTaskActivity.this.startActivity(intentMain);
+                }
+            }
+        };
+
+        btnEdit.setOnClickListener(handler);
+        btnClear.setOnClickListener(handler);
+        btnBack.setOnClickListener(handler);
+    }
 }
